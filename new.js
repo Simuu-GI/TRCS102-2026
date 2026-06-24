e932927f27e1bd0e93670dc03e6b0d142ddfbdcb
simuu <simran10sin@gmail.com>
2026-06-24 23:41:05 +0530

    day 1 updated

---
+++ new.js
@@ -0,0 +1,72 @@
+///alert('hello');
+console.log("hello");
+
+let username = "Simuu";
+console.log("username");
+
+username= "ABC";
+console.log(username);
+
+const PI = "3.14";
+
+var marks = 28;
+
+//data types
+
+let city = "Mohali";
+console.log(typeof city); // string
+
+let age = 20;
+console.log(typeof age); // number
+
+let isEven = true; //boolean
+console.log(typeof isEven);
+
+let person_name = null; //null
+console.log(person_name);
+
+let name; //undefined
+console.log(typeof name);
+
+// let my_name; //snake case
+// let myName; //camelcase
+
+//Arithematic operators
+
+let a= 10;
+let b= 20;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+//Assignment operaotrs
+
+let c = 5;
+c += 20;
+c -= 30;
+c /= 20;
+c *= 10;
+console.log(c);
+
+
+//logical operators
+
+age = 20;
+console.log(age<15 && age < 30);
+console.log(age<15 || age <50);
+
+let isOdd =true;
+console.log(! isOdd);
+
+//comparison operator
+
+let d= 2;
+let e= "2";
+console.log(d==e);
+console.log(d===e);
+
+
+
+
